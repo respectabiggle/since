dc0796b466ff8330a9507b998f983890363ba43c
respectabiggle <55676087+respectabiggle@users.noreply.github.com>
2023-09-30 01:35:12 -0400

    ...

--- since.js
+++ since.js
@@ -1,4 +1,4 @@
-import { log }			from 'console'
+import { log }				from 'console'
 import { coinbasepro }			from 'ccxt'
 import { config }			from 'dotenv'	; config()	
 
@@ -17,23 +17,23 @@ let sinceTest = async function() {
 
 
 	let symbol	= undefined
-	let since	= pro.parse8601 ('2018-01-01T00:00:00Z')			// 1514764800000		
+	let since	= pro.parse8601 ('2018-01-01T00:00:00Z')		// 1514764800000		
 	let params	= { since : 1514764800000 }
 	let foo
 
 	 
 	foo		= await pro.fetchOrders(symbol, since)			// 2021-04-18T02:58:10.640Z (symbol == undefined)
-	foo		= await pro.fetchOrders('ETH/USD', since)			// 2021-04-17T07:26:33.917Z 
-	// foo		= await pro.fetchOrders('ETH/USD', params)			// []
+	foo		= await pro.fetchOrders('ETH/USD', since)		// 2021-04-17T07:26:33.917Z 
+	// foo		= await pro.fetchOrders('ETH/USD', params)		// []
 	
 	 
 
 	foo		= await pro.fetchMyTrades('ETH/USD')			// 2021-05-27T19:27:08.517Z		
-	foo		= await pro.fetchMyTrades('ETH/USD', since)				// 2021-05-27T19:27:08.517Z confirmed	
+	foo		= await pro.fetchMyTrades('ETH/USD', since)		// 2021-05-27T19:27:08.517Z confirmed	
 	
 
 	
-	foo		= await pro.fetchLedger('ETH')			// 2021-06-04		
+	foo		= await pro.fetchLedger('ETH')				// 2021-06-04		
 	foo		= await pro.fetchLedger('ETH', since)			// 2021-06-04		
 	// foo		= await pro.fetchLedger('ETH', params)			// []		
 	
