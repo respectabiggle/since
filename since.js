d54105825fbb29f40c4afb72ab6d7773b909a42d
respectabiggle <55676087+respectabiggle@users.noreply.github.com>
2023-09-30 14:01:09 -0400

    formatting

--- since.js
+++ since.js
@@ -19,11 +19,11 @@ let sinceTest = async function() {
 
 
 	let symbol	= undefined
-		symbol	= 'ETH/USD'
+	symbol	= 'ETH/USD'
 	let since	= pro.parse8601 ('2018-01-01T00:00:00Z')	
-		since	= 1514764800000		
+	since	= 1514764800000		
 	let until	= pro.parse8601 ('2018-01-01T00:00:00Z')		
-		until	= 1514764800000	
+	until	= 1514764800000	
 	let params	= { since : 1514764800000, until : 1514764800000}
 
 
